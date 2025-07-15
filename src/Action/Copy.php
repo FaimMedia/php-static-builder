d06e6cafa02c08b6a68ee50352f5610eb75e356f
FaimMedia B.V. <FaimMedia@users.noreply.github.com>
2025-07-15 16:53:48 +0200

    Fixed using ignore with rsync and updated error handling

--- src/Action/Copy.php
+++ src/Action/Copy.php
@@ -15,11 +15,7 @@ class Copy extends AbstractAction implements ActionInterface
 	 */
 	public function execute(?bool $start = null): mixed
 	{
-		$path = $this->path ?? '';
-		if ($path && substr($path, -1) !== '/') {
-			$path .= '/';
-		}
-
+		$path = rtrim($this->path ?? '', '/') . '/';
 		$to = $this->target . $path;
 
 		if ($path && !file_exists($to)) {
@@ -29,8 +25,39 @@ class Copy extends AbstractAction implements ActionInterface
 		echo 'Copy' . PHP_EOL;
 		echo ' - ' . $this->source . ' -> ' . $to . PHP_EOL;
 
-		exec('cp -R ' . escapeshellarg($this->source) . ' ' . escapeshellarg($to));
+		if (!empty($this->ignore)) {
+			exec('which rsync', result_code: $code);
+			if ($code) {
+				throw new Exception('Using the `ignore` argument requires `rsync` to be installed');
+			}
+
+			$base = basename($this->source);
+			$exclude = array_map(function (string $dir) use ($base): string {
+				return '--exclude=' . escapeshellarg('/' . $base . '/' . $dir . '/');
+			}, $this->ignore);
+
+			$cmd = 'rsync -av ' . join(' ', $exclude)
+				. ' ' . escapeshellarg($this->source) . ' ' . escapeshellarg($to);
+
+			$this->exec($cmd);
+
+			return true;
+		}
+
+		$this->exec('cp -R ' . escapeshellarg($this->source) . ' ' . escapeshellarg($to));
 
 		return true;
+	}
+
+	/**
+	 * Run command and check exit code
+	 */
+	protected function exec(string $cmd): void
+	{
+		exec($cmd, $output, $code);
+
+		if ($code !== 0) {
+			throw new Exception("Error occured on copy:\r\n" . join("\r\n", $output));
+		}
 	}
 }
